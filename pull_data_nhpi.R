2531bbe8bf754128524976b37ed4cd805327d532
SunnyYShao <yshao006@ucr.edu>
2020-01-20 23:41:56 -0800

    adding nhpi data pulling script. exporting group specific shapefiles and city full list (within top 27 metros of AAPI community)

---
+++ pull_data_nhpi.R
@@ -0,0 +1,259 @@
+# load variables ----------------------------------------------------------
+library(tidycensus)
+library(tidyverse)
+library(stringr)
+
+# getting reference labels ------------------------------------------------
+label <- load_variables(year = 2017, dataset = "acs5", cache = TRUE) %>%
+  mutate(keep = str_detect(name, "B02016")) %>% filter(keep == "TRUE") %>%
+  separate(label, c("var1", "var2", "var3", "label"), sep = "!!") %>%
+  rename(variable = name) %>% dplyr::select(variable, label) %>% 
+  filter(!variable %in% c("B02016_005", "B02016_008", "B02016_010", "B02016_011", "B02016_012")) %>% 
+  mutate(variable = str_replace_all(variable, "B02016_", "A"))
+label[1,2] <- "NHPI"
+
+# list of state and counties where top MSAs locate ------------------------
+state_list <- c("CA", "CO", "PA", "NJ", "DE", "MD", "DC", "VA", 
+                "WV", "FL", "MO", "IL", "IN", "WI", "MA", "NH", 
+                "MI", "MN", "NY", "NC", "SC", "OR", "WA", "TX", 
+                "AZ", "HI", "AK", "AR", "UT", "GA", "NV")
+
+counties_nhpi <- read_csv("nhpi_countylists.csv") %>% 
+  rename(metro_label = NAMELSAD_2)
+
+counties <- read_csv("top_metro_ct.csv")
+
+metro_list <- counties %>% 
+  dplyr::select(NAMELSAD) %>%
+  rename(metro_label = NAMELSAD) %>% 
+  unique() %>% 
+  mutate(metro = as.numeric(row_number()))
+
+counties_nhpi <- counties_nhpi %>% 
+  left_join(metro_list) %>% 
+  mutate(metro = case_when(
+    metro_label == "Portland-Vancouver-Hillsboro, OR-WA Metro Area" ~27,
+    TRUE ~metro))
+
+
+counties_slim <- counties_nhpi %>% 
+  dplyr::select(-metro_label) %>% 
+  rename(ct_geoid = GEOID)
+
+top_ct <- as.vector(counties_slim$ct_geoid)
+
+# total pop at tract level ------------------------------------------------
+tot_tract <- get_acs(table = "B01003", year = 2017, geography = "tract", state = state_list, cache_table = T)
+tot_tract <- tot_tract %>% 
+  dplyr::select(GEOID, NAME, estimate) %>% 
+  rename(tot_pop = estimate)
+
+# pulling detailed AA tract-level data ------------------------------------
+nhpi_tract <- get_acs(table = "B02016", year = 2017, geography = "tract", state = state_list, summary_var = "B02016_001", cache_table = T)
+
+dta <- nhpi_tract %>% 
+  dplyr::select(GEOID, NAME, variable, estimate, summary_est) %>% 
+  filter(!variable %in% c("B02016_001", "B02016_005", "B02016_008", "B02016_010", "B02016_011", "B02016_012")) %>% 
+  mutate(variable = str_replace_all(variable, "B02016_", "A")) %>% 
+  left_join(tot_tract) %>% 
+  mutate(ct_geoid = substr(GEOID, 1, 5)) %>% 
+  filter(ct_geoid %in% top_ct) %>% 
+  left_join(counties_slim) %>% 
+  rename(pop = estimate,
+         nhpi_pop = summary_est) %>% 
+  mutate(pct_nhpi = round(pop / nhpi_pop, digits = 2),
+         pct_tot = round(pop / tot_pop, digits = 2)) %>% 
+  mutate(pct_nhpi = case_when(
+    nhpi_pop == 0 ~0,
+    pop == 0 ~0,
+    TRUE ~pct_nhpi)) %>% 
+  mutate(pct_tot = case_when(
+    tot_pop == 0 ~0,
+    pop ==0 ~0,
+    TRUE ~pct_tot)) %>% 
+  dplyr::select(-ct_geoid)
+
+#####Top NHPI groups in each census tract
+top_nhpi <- dta %>% 
+  group_by(GEOID) %>% 
+  arrange(desc(pop)) %>% 
+  mutate(rank = row_number()) %>% 
+  filter(rank == 1) %>% 
+  ungroup() %>% 
+  mutate(top1 = scales::comma(pop),
+         top2 = case_when(
+           is.na(pct_nhpi) == F ~paste(as.character(pct_nhpi*100), "%", sep = ""),
+           TRUE ~as.character(pct_nhpi))) %>% 
+  mutate(top3 = case_when(
+    is.na(pct_tot) == F ~paste(as.character(pct_tot*100), "%", sep = ""),
+    TRUE ~as.character(pct_tot))) %>% 
+  # dplyr::select(GEOID, NAME, variable, top1, top2, top3, pop, pct_asn, pct_tot, metro) %>% 
+  left_join(label) %>% dplyr::select(-variable)
+
+top_nhpi_map <- top_nhpi %>% 
+  dplyr::select(GEOID, NAME, label, top1, top2, top3, pop, pct_nhpi, pct_tot, metro) %>% 
+  mutate(metro = as.integer(metro)) %>% 
+  mutate(pop = as.integer(pop),
+         pct_nhpi = round(pct_nhpi, digits = 2),
+         pct_tot = round(pct_tot, digits = 2)) %>% 
+  mutate(pop = case_when(
+    top1 == "0" ~NA_integer_,
+    TRUE ~pop)) %>% 
+  mutate(pct_nhpi = case_when(
+    top2 == "0%" ~NA_real_,
+    TRUE ~pct_nhpi)) %>% 
+  mutate(pct_tot = case_when(
+    top3 == "0%" ~NA_real_,
+    TRUE ~pct_tot))
+  
+
+# generating group specific df --------------------------------------------
+dta <- dta %>% 
+  mutate(poplb = scales::comma(pop)) %>% 
+  mutate(pctlb1 = case_when(
+    is.na(pct_nhpi)==F ~paste(as.character(pct_nhpi*100), "%", sep = ""),
+    TRUE ~as.character(pct_nhpi))) %>% 
+  mutate(pctlb2 = case_when(
+    is.na(pct_tot)==F ~paste(as.character(pct_tot*100), "%", sep = ""),
+    TRUE ~as.character(pct_tot))) %>% 
+  dplyr::select(-nhpi_pop, -tot_pop, -NAMELSAD) %>% 
+  mutate(pop = case_when(
+    pop == 0 ~NA_real_,
+    TRUE ~pop)) %>% 
+  mutate(pct_nhpi = case_when(
+    pct_nhpi == 0 ~NA_real_,
+    TRUE ~pct_nhpi)) %>% 
+  mutate(pct_tot = case_when(
+    pct_tot == 0 ~NA_real_,
+    TRUE ~pct_tot)) %>% 
+  mutate(metro = as.integer(metro),
+         pop = as.integer(pop),
+         pct_nhpi = round(pct_nhpi, digits = 2),
+         pct_tot = round(pct_tot, digits = 2))
+
+
+# group specific layers ---------------------------------------------------
+
+#group 2
+dta_nhpi2 <- dta %>% 
+  filter(variable == "A002") %>%
+  rename(pop2 = pop,
+         Ap2 = pct_nhpi,
+         Tp2 = pct_tot) %>% 
+  left_join(top_nhpi_map) %>% 
+  dplyr::select(-variable)
+
+#group 3
+dta_nhpi3 <- dta %>% 
+  filter(variable == "A003") %>%
+  rename(pop2 = pop,
+         Ap2 = pct_nhpi,
+         Tp2 = pct_tot) %>% 
+  left_join(top_nhpi_map) %>% 
+  dplyr::select(-variable)
+
+#group 4
+dta_nhpi4 <- dta %>% 
+  filter(variable == "A004") %>%
+  rename(pop2 = pop,
+         Ap2 = pct_nhpi,
+         Tp2 = pct_tot) %>% 
+  left_join(top_nhpi_map) %>% 
+  dplyr::select(-variable)
+
+#group 6
+dta_nhpi6 <- dta %>% 
+  filter(variable == "A006") %>%
+  rename(pop2 = pop,
+         Ap2 = pct_nhpi,
+         Tp2 = pct_tot) %>% 
+  left_join(top_nhpi_map) %>% 
+  dplyr::select(-variable)
+
+#group 7
+dta_nhpi7 <- dta %>% 
+  filter(variable == "A007") %>%
+  rename(pop2 = pop,
+         Ap2 = pct_nhpi,
+         Tp2 = pct_tot) %>% 
+  left_join(top_nhpi_map) %>% 
+  dplyr::select(-variable)
+
+#group 9
+dta_nhpi9 <- dta %>% 
+  filter(variable == "A009") %>%
+  rename(pop2 = pop,
+         Ap2 = pct_nhpi,
+         Tp2 = pct_tot) %>% 
+  left_join(top_nhpi_map) %>% 
+  dplyr::select(-variable)
+
+# getting tract-level shapefile -------------------------------------------
+library(tigris)
+options(tigris_use_cache = T)
+
+library(rappdirs)
+user_cache_dir()
+
+library(GISTools)
+library(rgdal)
+# install.packages("rmapshaper")  # install if necessary
+library(sf)
+library(rmapshaper)
+# install.packages("rgeos")
+library(rgeos)
+# devtools::install_github('walkerke/tigris')
+# rm(combined, combined_generalized)
+
+
+combined <- rbind_tigris(lapply(state_list, function(x){
+  tracts(x, cb = TRUE, refresh = TRUE)}))
+
+#getting city broundaries shapefile
+cities <- rbind_tigris(lapply(state_list, function(x){
+  places(x, cb = TRUE, refresh = TRUE)}))
+city_generalized = ms_simplify(cities, keep = 0.05)
+writeOGR(obj=city_generalized, dsn = "/Users/sunnyshao/Documents/census2020_planning/cit_nhpi", layer="cit_nhpi", driver="ESRI Shapefile") # this is in geographical projection
+
+# combined_generalized <- gSimplify(combined, 0.4, topologyPreserve=TRUE)
+combined_generalized = ms_simplify(combined, keep = 0.05)
+
+# top NHPI in each metro area ------------------------------------
+merge <- geo_join(combined_generalized, top_nhpi_map, "GEOID", "GEOID")
+writeOGR(obj=merge, dsn="/Users/sunnyshao/Dropbox/AAPI DATA Desktop/census planning/topNHPI", layer="toppi", driver="ESRI Shapefile") # this is in geographical projection
+##############
+
+# NHPI in each metro area ------------------------------------
+
+#group 2
+merge <- geo_join(combined_generalized, dta_nhpi2, "GEOID", "GEOID")
+writeOGR(obj=merge, dsn="/Users/sunnyshao/Dropbox/AAPI DATA Desktop/census planning/pi02", layer="pi02", driver="ESRI Shapefile") # this is in geographical projection
+
+#group 3
+merge <- geo_join(combined_generalized, dta_nhpi3, "GEOID", "GEOID")
+writeOGR(obj=merge, dsn="/Users/sunnyshao/Dropbox/AAPI DATA Desktop/census planning/pi03", layer="pi03", driver="ESRI Shapefile") # this is in geographical projection
+
+#group 4
+merge <- geo_join(combined_generalized, dta_nhpi4, "GEOID", "GEOID")
+writeOGR(obj=merge, dsn="/Users/sunnyshao/Dropbox/AAPI DATA Desktop/census planning/pi04", layer="pi04", driver="ESRI Shapefile") # this is in geographical projection
+
+#group 6
+merge <- geo_join(combined_generalized, dta_nhpi6, "GEOID", "GEOID")
+writeOGR(obj=merge, dsn="/Users/sunnyshao/Dropbox/AAPI DATA Desktop/census planning/pi06", layer="pi06", driver="ESRI Shapefile") # this is in geographical projection
+
+#group 7
+merge <- geo_join(combined_generalized, dta_nhpi7, "GEOID", "GEOID")
+writeOGR(obj=merge, dsn="/Users/sunnyshao/Dropbox/AAPI DATA Desktop/census planning/pi07", layer="pi07", driver="ESRI Shapefile") # this is in geographical projection
+
+#group 9
+merge <- geo_join(combined_generalized, dta_nhpi9, "GEOID", "GEOID")
+writeOGR(obj=merge, dsn="/Users/sunnyshao/Dropbox/AAPI DATA Desktop/census planning/pi09", layer="pi09", driver="ESRI Shapefile") # this is in geographical projection
+
+
+
+
+
+
+
+
+
--- pulling_data.R
+++ pulling_data.R
@@ -29,7 +29,7 @@ library(stringr)
 label <- load_variables(year = 2017, dataset = "acs5", cache = TRUE) %>%
   mutate(keep = str_detect(name, "B02015")) %>% filter(keep == "TRUE") %>%
   separate(label, c("var1", "var2","label"), sep = "!!") %>%
-  rename(variable = name) %>% select(variable, label) %>% 
+  rename(variable = name) %>% dplyr::select(variable, label) %>% 
   filter(!variable %in% c("B02015_023", "B02015_024", "B02015_025")) %>% 
   mutate(variable = str_replace_all(variable, "B02015_", "A"))
 label[1,2] <- "Asian Am"
@@ -38,32 +38,42 @@ label[7,2] <- "Chinese"
 # list of state and counties where top MSAs locate ------------------------
 state_list <- c("CA", "CO", "PA", "NJ", "DE", "MD", "DC", "VA", 
                 "WV", "FL", "MO", "IL", "IN", "WI", "MA", "NH", 
-                "MI", "MN", "NY", "NC", "SC", "OR", "WA", "TX")
-counties <- read_csv("ct_topmsa.csv")
+                "MI", "MN", "NY", "NC", "SC", "OR", "WA", "TX", 
+                "AZ", "HI", "AK", "AR", "UT", "GA")
+
+counties <- read_csv("top_metro_ct.csv")
 
 metro_list <- counties %>% 
-  select(NAMELSAD_2) %>% 
+  dplyr::select(NAMELSAD) %>% 
   unique() %>% 
   mutate(metro = row_number())
 
-write_csv(metro_list, "metro_code_list.csv", na = "")
-
-counties_slim <- counties %>% 
-  select(GEOID, NAMELSAD_2) %>% 
-  rename(ct_geoid = GEOID) %>% 
+metro_geo <- read_csv("/Users/sunnyshao/Dropbox/AAPI DATA Desktop/census planning/topmetro_geoid.csv") %>% 
   left_join(metro_list)
 
-top_ct <- as.vector(counties$GEOID)
+write_csv(metro_geo, "metro_geo.csv", na = "")
+
+counties_slim <- counties %>% 
+  dplyr::select(-NAMELSAD_2) %>% 
+  rename(ct_geoid = GEOID_2,
+         asnP = top_metro_asnP,
+         metroP = top_metro_metroP,
+         nhpiP = top_metro_nhpiP) %>% 
+  left_join(metro_list) %>% 
+  dplyr::select(-NAMELSAD)
+
+top_ct <- as.vector(counties_slim$ct_geoid)
 
 
 # total pop at tract level ------------------------------------------------
-tot_tract <- get_acs(table = "B01003", year = 2017, geography = "tract", state = state_list)
+tot_tract <- get_acs(table = "B01003", year = 2017, geography = "tract", state = state_list, cache_table = T)
 tot_tract <- tot_tract %>% 
   select(GEOID, NAME, estimate) %>% 
   rename(tot_pop = estimate)
 
 # pulling detailed AA tract-level data ------------------------------------
-asn_tract <- get_acs(table = "B02015", year = 2017, geography = "tract", state = state_list, summary_var = "B02015_001")
+asn_tract <- get_acs(table = "B02015", year = 2017, geography = "tract", state = state_list, summary_var = "B02015_001", cache_table = T)
+nhpi_tract <- get_acs(table = "B02016", year = 2017, geography = "tract", state = state_list, summary_var = "B02015_001", cache_table = T)
 
 dta <- asn_tract %>% 
   dplyr::select(GEOID, NAME, variable, estimate, summary_est) %>% 
@@ -73,7 +83,7 @@ dta <- asn_tract %>%
   mutate(ct_geoid = substr(GEOID, 1, 5)) %>% 
   filter(ct_geoid %in% top_ct) %>% 
   left_join(counties_slim) %>% 
-  dplyr::select(-NAMELSAD_2) %>% 
+  # dplyr::select(-ct_geoid) %>% 
   rename(pop = estimate,
          asn_pop = summary_est) %>% 
   mutate(pct_asn = round(pop / asn_pop, digits = 2),
@@ -84,8 +94,9 @@ dta <- asn_tract %>%
   mutate(pct_tot = case_when(
     tot_pop == 0 ~NA_real_,
     TRUE ~pct_tot)) %>% 
-  dplyr::select(-ct_geoid, -asn_pop, -tot_pop)
+  dplyr::select(-ct_geoid)
 
+#######
 top_asn <- dta %>% 
   group_by(GEOID) %>% 
   arrange(desc(pop)) %>% 
@@ -98,10 +109,11 @@ top_asn <- dta %>%
   mutate(top3 = case_when(
            is.na(pct_tot) == F ~paste(as.character(pct_tot*100), "%", sep = ""),
            TRUE ~as.character(pct_tot))) %>% 
-  dplyr::select(GEOID, NAME, variable, top1, top2, top3, pop, pct_asn, pct_tot, metro) %>% 
+  # dplyr::select(GEOID, NAME, variable, top1, top2, top3, pop, pct_asn, pct_tot, metro) %>% 
   left_join(label) %>% dplyr::select(-variable)
 
-# top_asn <- top_asn_final %>% select(-pop, -pct_asn, -pct_tot)
+top_asn_map <- top_asn %>% 
+  dplyr::select(GEOID, NAME, label, top1, top2, top3, pop, pct_asn, pct_tot, metro)
 
 # generating group specific df --------------------------------------------
 dta <- dta %>% 
@@ -111,88 +123,95 @@ dta <- dta %>%
     TRUE ~as.character(pct_asn))) %>% 
   mutate(pctlb2 = case_when(
     is.na(pct_tot)==F ~paste(as.character(pct_tot*100), "%", sep = ""),
-    TRUE ~as.character(pct_tot)))
+    TRUE ~as.character(pct_tot))) %>% 
+  dplyr::select(-asn_pop, -tot_pop, -metroP, -asnP, -nhpiP)
 
 #group 2
 dta_asn2 <- dta %>% 
-  filter(variable == "A002") %>% 
-  left_join(top_asn) %>% 
-  dplyr::select(-variable) %>% 
+  filter(variable == "A002") %>%
   rename(pop2 = pop,
          Ap2 = pct_asn,
-         Tp2 = pct_tot)
+         Tp2 = pct_tot) %>% 
+  left_join(top_asn_map) %>% 
+  dplyr::select(-variable)
+
 #group 3
 dta_asn3 <- dta %>% 
   filter(variable == "A003") %>% 
-  left_join(top_asn) %>% 
-  dplyr::select(-variable) %>% 
-  rename(pop3 = pop,
-         Ap3 = pct_asn,
-         Tp3 = pct_tot)
+  rename(pop2 = pop,
+         Ap2 = pct_asn,
+         Tp2 = pct_tot) %>% 
+  left_join(top_asn_map) %>% 
+  dplyr::select(-variable)
 #group 4
 dta_asn4 <- dta %>% 
   filter(variable == "A004") %>% 
-  left_join(top_asn) %>% 
-  dplyr::select(-variable) %>% 
-  rename(pop4 = pop,
-         Ap4 = pct_asn,
-         Tp4 = pct_tot)
+  rename(pop2 = pop,
+         Ap2 = pct_asn,
+         Tp2 = pct_tot) %>% 
+  left_join(top_asn_map) %>% 
+  dplyr::select(-variable)
 #group 5
 dta_asn5 <- dta %>% 
   filter(variable == "A005") %>% 
-  left_join(top_asn) %>% 
-  dplyr::select(-variable) %>% 
-  rename(pop5 = pop,
-         Ap5 = pct_asn,
-         Tp5 = pct_tot)
+  rename(pop2 = pop,
+         Ap2 = pct_asn,
+         Tp2 = pct_tot) %>% 
+  left_join(top_asn_map) %>% 
+  dplyr::select(-variable)
 #group 6
 dta_asn6 <- dta %>% 
   filter(variable == "A006") %>% 
-  left_join(top_asn) %>% 
-  dplyr::select(-variable) %>% 
-  rename(pop6 = pop,
-         Ap6 = pct_asn,
-         Tp6 = pct_tot)
+  rename(pop2 = pop,
+         Ap2 = pct_asn,
+         Tp2 = pct_tot) %>% 
+  left_join(top_asn_map) %>% 
+  dplyr::select(-variable)
 #group 7
 dta_asn7 <- dta %>% 
   filter(variable == "A007") %>% 
-  left_join(top_asn) %>% 
-  dplyr::select(-variable) %>% 
-  rename(pop7 = pop,
-         Ap7 = pct_asn,
-         Tp7 = pct_tot)
+  rename(pop2 = pop,
+         Ap2 = pct_asn,
+         Tp2 = pct_tot) %>% 
+  left_join(top_asn_map) %>% 
+  dplyr::select(-variable)
+
 #group 8
 dta_asn8 <- dta %>% 
   filter(variable == "A008") %>% 
-  left_join(top_asn) %>% 
-  dplyr::select(-variable) %>% 
-  rename(pop8 = pop,
-         Ap8 = pct_asn,
-         Tp8 = pct_tot)
+  rename(pop2 = pop,
+         Ap2 = pct_asn,
+         Tp2 = pct_tot) %>% 
+  left_join(top_asn_map) %>% 
+  dplyr::select(-variable)
+
 #group 9
 dta_asn9 <- dta %>% 
   filter(variable == "A009") %>% 
-  left_join(top_asn) %>% 
-  dplyr::select(-variable) %>% 
-  rename(pop9 = pop,
-         Ap9 = pct_asn,
-         Tp9 = pct_tot)
+  rename(pop2 = pop,
+         Ap2 = pct_asn,
+         Tp2 = pct_tot) %>% 
+  left_join(top_asn_map) %>% 
+  dplyr::select(-variable)
+
 #group 10
 dta_asn10 <- dta %>% 
   filter(variable == "A010") %>% 
-  left_join(top_asn) %>% 
-  dplyr::select(-variable) %>% 
-  rename(pop10 = pop,
-         Ap10 = pct_asn,
-         Tp10 = pct_tot)
+  rename(pop2 = pop,
+         Ap2 = pct_asn,
+         Tp2 = pct_tot) %>% 
+  left_join(top_asn_map) %>% 
+  dplyr::select(-variable)
+
 #group 11
 dta_asn11 <- dta %>% 
   filter(variable == "A011") %>% 
-  left_join(top_asn) %>% 
-  dplyr::select(-variable) %>% 
-  rename(pop11 = pop,
-         Ap11 = pct_asn,
-         Tp11 = pct_tot)
+  rename(pop2 = pop,
+         Ap2 = pct_asn,
+         Tp2 = pct_tot) %>% 
+  left_join(top_asn_map) %>% 
+  dplyr::select(-variable)
+
 #group 12
 dta_asn12 <- dta %>% 
   filter(variable == "A012") %>% 
@@ -204,75 +223,84 @@ dta_asn12 <- dta %>%
 #group 13
 dta_asn13 <- dta %>% 
   filter(variable == "A013") %>% 
-  left_join(top_asn) %>% 
-  dplyr::select(-variable) %>% 
-  rename(pop13 = pop,
-         Ap13 = pct_asn,
-         Tp13 = pct_tot)
+  rename(pop2 = pop,
+         Ap2 = pct_asn,
+         Tp2 = pct_tot) %>% 
+  left_join(top_asn_map) %>% 
+  dplyr::select(-variable)
+
 #group 14
 dta_asn14 <- dta %>% 
   filter(variable == "A014") %>% 
-  left_join(top_asn) %>% 
-  dplyr::select(-variable) %>% 
-  rename(pop14 = pop,
-         Ap14 = pct_asn,
-         Tp14 = pct_tot)
+  rename(pop2 = pop,
+         Ap2 = pct_asn,
+         Tp2 = pct_tot) %>% 
+  left_join(top_asn_map) %>% 
+  dplyr::select(-variable)
+
 #group 15
 dta_asn15 <- dta %>% 
   filter(variable == "A015") %>% 
-  left_join(top_asn) %>% 
-  dplyr::select(-variable) %>% 
-  rename(pop15 = pop,
-         Ap15 = pct_asn,
-         Tp15 = pct_tot)
+  rename(pop2 = pop,
+         Ap2 = pct_asn,
+         Tp2 = pct_tot) %>% 
+  left_join(top_asn_map) %>% 
+  dplyr::select(-variable)
+
 #group 16
 dta_asn16 <- dta %>% 
   filter(variable == "A016") %>% 
-  left_join(top_asn) %>% 
-  dplyr::select(-variable) %>% 
-  rename(pop16 = pop,
-         Ap16 = pct_asn,
-         Tp16 = pct_tot)
+  rename(pop2 = pop,
+         Ap2 = pct_asn,
+         Tp2 = pct_tot) %>% 
+  left_join(top_asn_map) %>% 
+  dplyr::select(-variable)
+
 
 dta_asn2 <- dta %>% 
   filter(variable == "A002") %>% 
-  left_join(top_asn) %>% 
-  dplyr::select(-variable) %>% 
   rename(pop2 = pop,
          Ap2 = pct_asn,
-         Tp2 = pct_tot)
+         Tp2 = pct_tot) %>% 
+  left_join(top_asn_map) %>% 
+  dplyr::select(-variable)
+
 #group 17
 dta_asn17 <- dta %>% 
   filter(variable == "A017") %>% 
-  left_join(top_asn) %>% 
-  dplyr::select(-variable) %>% 
-  rename(pop17 = pop,
-         Ap17 = pct_asn,
-         Tp17 = pct_tot)
+  rename(pop2 = pop,
+         Ap2 = pct_asn,
+         Tp2 = pct_tot) %>% 
+  left_join(top_asn_map) %>% 
+  dplyr::select(-variable)
+
 #group 18
 dta_asn18 <- dta %>% 
   filter(variable == "A018") %>% 
-  left_join(top_asn) %>% 
-  dplyr::select(-variable) %>% 
-  rename(pop18 = pop,
-         Ap18 = pct_asn,
-         Tp18 = pct_tot)
+  rename(pop2 = pop,
+         Ap2 = pct_asn,
+         Tp2 = pct_tot) %>% 
+  left_join(top_asn_map) %>% 
+  dplyr::select(-variable)
+
 #group 19
 dta_asn19 <- dta %>% 
   filter(variable == "A019") %>% 
-  left_join(top_asn) %>% 
-  dplyr::select(-variable) %>% 
-  rename(pop19 = pop,
-         Ap19 = pct_asn,
-         Tp19 = pct_tot)
+  rename(pop2 = pop,
+         Ap2 = pct_asn,
+         Tp2 = pct_tot) %>% 
+  left_join(top_asn_map) %>% 
+  dplyr::select(-variable)
+
 #group 20
 dta_asn20 <- dta %>% 
   filter(variable == "A020") %>% 
-  left_join(top_asn) %>% 
-  dplyr::select(-variable) %>% 
-  rename(pop20 = pop,
-         Ap20 = pct_asn,
-         Tp20 = pct_tot)
+  rename(pop2 = pop,
+         Ap2 = pct_asn,
+         Tp2 = pct_tot) %>% 
+  left_join(top_asn_map) %>% 
+  dplyr::select(-variable)
+
 #group 21
 dta_asn21 <- dta %>% 
   filter(variable == "A021") %>% 
@@ -284,37 +312,61 @@ dta_asn21 <- dta %>%
 #group 22
 dta_asn22 <- dta %>% 
   filter(variable == "A022") %>% 
-  left_join(top_asn) %>% 
-  dplyr::select(-variable) %>% 
-  rename(pop22 = pop,
-         Ap22 = pct_asn,
-         Tp22 = pct_tot)
+  rename(pop2 = pop,
+         Ap2 = pct_asn,
+         Tp2 = pct_tot) %>% 
+  left_join(top_asn_map) %>% 
+  dplyr::select(-variable)
+
 
 
 # getting tract-level shapefile -------------------------------------------
 library(tigris)
+options(tigris_use_cache = T)
+
+library(rappdirs)
+user_cache_dir()
+
 library(GISTools)
 library(rgdal)
 # install.packages("rmapshaper")  # install if necessary
-library("sf")
-library("rmapshaper")
+library(sf)
+library(rmapshaper)
 # install.packages("rgeos")
 library(rgeos)
-
+# devtools::install_github('walkerke/tigris')
 # rm(combined, combined_generalized)
 
 combined <- rbind_tigris(lapply(state_list, function(x){
-  tracts(x, cb = TRUE)}))
+  tracts(x, cb = TRUE, refresh = TRUE)}))
+
 # combined_generalized <- gSimplify(combined, 0.4, topologyPreserve=TRUE)
 combined_generalized = ms_simplify(combined, keep = 0.09)
 
+#writeOGR(obj=combined_generalized, dsn = "/Users/sunnyshao/Documents/census2020_planning", layer="test", driver="ESRI Shapefile") # this is in geographical projection
+
+
+#getting city broundaries shapefile
+cities <- rbind_tigris(lapply(state_list, function(x){
+  places(x, cb = TRUE, refresh = TRUE)}))
+
+# combined_generalized <- gSimplify(combined, 0.4, topologyPreserve=TRUE)
+city_generalized = ms_simplify(cities, keep = 0.05)
+writeOGR(obj=city_generalized, dsn = "/Users/sunnyshao/Documents/census2020_planning/cit_nhpi", layer="cit", driver="ESRI Shapefile") # this is in geographical projection
+
+##############
 
 #read in shapefile
 
-#test
+#top Asian Am in each metro area
+merge <- geo_join(combined_generalized, top_asn_map, "GEOID", "GEOID")
+writeOGR(obj=merge, dsn="/Users/sunnyshao/Dropbox/AAPI DATA Desktop/census planning/topAA", layer="topaa", driver="ESRI Shapefile") # this is in geographical projection
+##############
+
+#Asian Group 2 - Asian Indian
 merge <- geo_join(combined_generalized, dta_asn2, "GEOID", "GEOID")
 writeOGR(obj=merge, dsn="/Users/sunnyshao/Dropbox/AAPI DATA Desktop/census planning/aa2", layer="aa2", driver="ESRI Shapefile") # this is in geographical projection
-##############
+
 
 merge <- geo_join(combined_generalized, top_asn, "GEOID", "GEOID")
 writeOGR(obj=merge, dsn="/Users/sunnyshao/Dropbox/AAPI DATA Desktop/census planning/topasn", layer="topaa", driver="ESRI Shapefile") # this is in geographical projection
@@ -380,6 +432,11 @@ writeOGR(obj = merge, dsn = "/Users/sunnyshao/Dropbox/AAPI DATA Desktop/census p
 merge <- geo_join(combined_generalized, dta_asn22, "GEOID", "GEOID")
 writeOGR(obj = merge, dsn = "/Users/sunnyshao/Dropbox/AAPI DATA Desktop/census planning/aa22", layer = "aa22", driver = "ESRI Shapefile")
 
+rm(dta_asn2, dta_asn3, dta_asn4, dta_asn5, dta_asn6,
+   dta_asn7, dta_asn8, dta_asn9, dta_asn10, dta_asn11,
+   dta_asn12, dta_asn13, dta_asn14, dta_asn15, dta_asn16,
+   dta_asn17, dta_asn18, dta_asn19, dta_asn20, dta_asn21,
+   dta_asn22, combined_generalized, combined, merge)
 ###
 sum <- dta %>% 
   filter(metro == 19) %>% 
@@ -398,4 +455,6 @@ sum <- dta %>%
 #save large files
 write_csv(tot_tract, "/Users/sunnyshao/Dropbox/AAPI DATA Desktop/census planning/tot_tract.csv", na= "")
 write_csv(asn_tract, "/Users/sunnyshao/Dropbox/AAPI DATA Desktop/census planning/asn_tract.csv", na = "")
-  
+
+stats <- top_asn %>% 
+  filter(metro == "17")
